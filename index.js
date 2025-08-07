b61dc86fcfefb6a682d73d75e7e1d48de508d66c
Nisha Bairagi <nishabairagi9196@gmail.com>
2025-08-07 19:38:27 +0530

    update index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
- 
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./db/config.js"); 
+require("dotenv").config();
+const connectDB = require("./db/config.js");
 
 const roleControl = require("./controls/rolecontrol.js");
 const patientControl = require("./controls/patientControl.js");
 const departmentControl = require("./controls/DepartmentControl.js");
 const appointmentControl = require("./controls/AppointmentControl.js");
-const BillingControl = require("./controls/BillingControl.js");
+
 const userRoutes = require("./controls/RegistrationControl.js");
 
 const app = express();
@@ -21,10 +21,10 @@ console.log("🔌 Calling connectDB()...");
 app.use("/api", roleControl);
 app.use("/api", departmentControl);
 app.use("/api", appointmentControl);
-app.use("/api", BillingControl);
+
 app.use("/api", userRoutes);
 app.use("/api", patientControl);
-app.use("/upload", express.static('upload'));
+app.use("/upload", express.static("upload"));
 connectDB().then(() => {
   const port = process.env.PORT || 8080;
   app.listen(port, () => {
